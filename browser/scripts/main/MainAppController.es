03020abf1c096df23c9433f2c32f3c04b5a5e08d
showc7 <showc7@gmail.com>
2015-10-23 10:35:06 +0300

    add request to another domain

--- browser/scripts/main/MainAppController.es
+++ browser/scripts/main/MainAppController.es
@@ -1,17 +1,22 @@
 //import Requests from './components/requests.es'
 
 class MainAppController {
-   constructor ($scope) {
+   constructor ($scope,$http) {
       console.log('ok');
       this.$scope = $scope;
+      this.$http = $http;
       this.initalize();
    }
 
    initalize() {
       console.log('initializing');
       this.$scope.text = 'Hello world!';
+      var self = this;
+      this.$http.get('/api/app/test').success(function (data) {
+         self.$scope.text = data;
+      });
    }
 }
 
-MainAppController.$inject = ['$scope'];
+MainAppController.$inject = ['$scope','$http'];
 export { MainAppController }
--- server/controller/app/test.js
+++ server/controller/app/test.js
@@ -1,4 +1,29 @@
+var http = require('http');
+
 exports.index = function(req, res, next, page, second) {
-    res.send('Page: ' + page + ' Second: ' + second);
-    res.end();
+    //res.send('Page: ' + page + ' Second: ' + second);
+
+    var options = {
+      host: 'habrahabr.ru',
+      path: '/rss/interesting/'
+    };
+
+    callback = function(response) {
+      var str = '';
+
+      //another chunk of data has been recieved, so append it to `str`
+      response.on('data', function (chunk) {
+        str += chunk;
+      });
+
+      //the whole response has been recieved, so we just print it out here
+      response.on('end', function () {
+        console.log(str);
+        res.send(str);
+        res.end();
+     });
+
+    }
+
+    http.request(options, callback).end();
 };
