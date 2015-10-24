cfb21f3cae78bd2663846a7bc907c0ed51b3a975
showc7 <showc7@gmail.com>
2015-10-25 00:03:42 +0300

    api addToFeedsList

--- server/controller/app/feeds.js
+++ server/controller/app/feeds.js
@@ -6,14 +6,33 @@ exports.index = function(req, res, next, page, second) {
       res.send(data);
       res.end();
    });
+   addToFeedsList({
+      name: 'a',
+      url: 'www.ololo.com/feed'
+   });
 };
 
 getFeedsList = function(callback) {
    var db = new PouchDB('http://localhost:5984/feed_urls');
    db.allDocs({include_docs: true, descending: true}, function(err, doc) {
-    //redrawTodosUI(doc.rows);
     console.log(doc.rows);
     data = doc.rows;
     callback(data);
+   });
+}
+
+addToFeedsList = function(data) {
+   var db = new PouchDB('http://localhost:5984/feed_urls');
+   db.get(data.url).then(function () {
+      // exists
+      console.log('exists');
+      return;
+   }).catch(function(err) {
+      // do not exists
+      console.log('not exists');
+      db.put({
+         _id: data.url,
+         name: data.name,
+      });
    });
 }
