7bcfc88b7b337a14cc14df12252c3fb7663e2eea
Vervolk22 <Bozon22@gmail.com>
2015-10-25 00:12:20 +0300

    Update feedData.js
    Made put and update of feedData to couchDB.

--- server/controller/feedData/feedData.js
+++ server/controller/feedData/feedData.js
@@ -17,20 +17,36 @@ getData = function(url,callback) {
    console.log('getData');
    db.get(url).then(function (doc) {
       console.log('cached');
+      now = new Date();
+      console.log(doc);
+      if(doc.date + 5 * 60 * 1000 <= now.getTime()) {
+         performRequest(db,url,callback,doc._rev);
+         return;
+      }
       callback(doc.responseData);
    }).catch(function (err) {
-      console.log('missed');
-      request("http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=" + url, function(error, response, body) {
-         var data = (JSON.parse(body)).responseData;
-         db.put({
-            _id: url,
-            responseData: data
-         }).then(function(response){
-            console.log('response ' + response);
-         }).catch(function(err){
-            console.log('err' + err);
-         });
-         callback(data);
+      console.log('missedD');
+      performRequest(db,url,callback,null);
+   });
+}
+
+performRequest = function(db,url,callback, rev) {
+   request("http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=" + url, function(error, response, body) {
+      var data = (JSON.parse(body)).responseData;
+      newDoc = {
+         _id: url,
+         responseData: data,
+         date: new Date().getTime()
+      };
+      if(rev !== null) {
+         newDoc._rev = rev;
+      }
+console.log(newDoc);
+      db.put(newDoc).then(function(response){
+         console.log('response ' + response);
+      }).catch(function(err){
+         console.log('err' + err);
       });
+      callback(data);
    });
 }
