99e0598859d1eb9afa83fc009a8c40e7945c39c2
showc7 <showc7@gmail.com>
2015-10-24 18:13:14 +0300

    api feedData created

---
+++ server/controller/app/feedData.js
@@ -0,0 +1,15 @@
+var request = require("request")
+
+exports.index = function(req, res, url) {
+    //url = 'http://habrahabr.ru/rss/interesting/';
+    getData(url,function (data) {
+      res.send(data);
+      res.end();
+   });
+};
+
+getData = function(url,callback) {
+   request("http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=http://habrahabr.ru/rss/interesting/", function(error, response, body) {
+      callback((JSON.parse(body)).responseData);
+   });
+}
