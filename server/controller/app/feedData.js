89d6be86ef455f5c372664659dde3b15f2f12060
showc7 <showc7@gmail.com>
2015-10-25 08:50:53 +0300

    add num=20 to google api request

--- server/controller/app/feedData.js
+++ server/controller/app/feedData.js
@@ -9,7 +9,7 @@ exports.index = function(req, res, url) {
 };
 
 getData = function(url,callback) {
-   request("http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=http://habrahabr.ru/rss/interesting/", function(error, response, body) {
+   request("http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=20&q=http://habrahabr.ru/rss/interesting/", function(error, response, body) {
       callback((JSON.parse(body)).responseData);
    });
 }
